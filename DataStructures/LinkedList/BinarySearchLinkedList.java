f12097a87e5ef1a28c48e9b7802dfa8a6f4ac22a
abhishek <akumar074@gmail.com>
2019-09-06 00:58:01 +0530

    Added Binary search implementation in a singly linked list with iteration

--- DataStructures/LinkedList/BinarySearchLinkedList.java
+++ DataStructures/LinkedList/BinarySearchLinkedList.java
@@ -6,12 +6,14 @@ public class BinarySearchLinkedList {
     for (int i = 1; i <= 10; i++){
       linkedList.insertAtEnd(new ListNode(i));
     }
-    int givenValue = 5;
+    int givenValue = 1;
     System.out.println("Q. Is " + givenValue + " in the Linked List " + linkedList.toString() + " ?");
-    System.out.println("Answer: " + binarySearch(linkedList.head, givenValue));
+    System.out.println("Answer: " + binarySearchWithIterator(linkedList.head, givenValue));
   }
 
   private static ListNode findMiddle(ListNode head){
+    if(head == null)
+      return null;
     ListNode slow = head, fast = head;
     while(fast != null && fast.getNext() != null) {
       slow = slow.getNext();
@@ -34,5 +36,36 @@ public class BinarySearchLinkedList {
     } else {
       return binarySearch(middle, givenValue);
     }
+  }
+
+  private static boolean binarySearchWithIterator(ListNode head, int givenValue){
+    ListNode start = head, last = null;
+    do {
+      ListNode middle = findMiddle(start, last);
+      System.out.println(middle.getData());
+      if(start.getData() == givenValue || middle.getData() == givenValue){
+        return true;
+      }
+      if(start.next == last || middle == null){
+        return false;
+      }
+      if(givenValue < middle.getData())
+        last = middle;
+      else if(givenValue > middle.getData())
+        start = middle.getNext();
+    } while ( last != null || last != start);
+    return false;
+  }
+
+  private static ListNode findMiddle(ListNode start, ListNode last){
+    if (start == null) {
+      return null;
+    }
+    ListNode slow = start, fast = start;
+    while(fast != last && fast.getNext() != last) {
+      slow = slow.getNext();
+      fast = fast.getNext().getNext();
+    }
+    return slow;
   }
 }
